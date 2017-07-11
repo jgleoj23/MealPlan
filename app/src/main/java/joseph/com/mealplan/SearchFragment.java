455d9eb76ab1e81a4611dea86c9de8e99d79d1a6
Joseph Gardi <jgardi@fb.com>
2017-07-11 15:34:03 -0700

    Removed the action bar

---
+++ app/src/main/java/joseph/com/mealplan/SearchFragment.java
@@ -0,0 +1,18 @@
+package joseph.com.mealplan;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Joseph Gardi
+ */
+public class SearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
