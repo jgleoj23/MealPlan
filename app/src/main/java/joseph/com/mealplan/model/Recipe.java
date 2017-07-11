b3045ccdba31cd1302172e1c97cb6a8030b6f992
Joseph Gardi <jgardi@fb.com>
2017-07-11 14:15:20 -0700

    Initial commit

---
+++ app/src/main/java/joseph/com/mealplan/model/Recipe.java
@@ -0,0 +1,16 @@
+package joseph.com.mealplan.model;
+
+/**
+ * @author Joseph Gardi
+ */
+public class Recipe {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
