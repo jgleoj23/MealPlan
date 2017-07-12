43ad262e0e5fe9c30123d970e762e79651bb3499
Karen Guo <kcguo@fb.com>
2017-07-12 11:30:46 -0700

    added recipe description scrollview

--- app/src/main/java/joseph/com/mealplan/RecipeDetailsActivity.java
+++ app/src/main/java/joseph/com/mealplan/RecipeDetailsActivity.java
@@ -2,13 +2,34 @@ package joseph.com.mealplan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import joseph.com.mealplan.model.Recipe;
 
 public class RecipeDetailsActivity extends AppCompatActivity {
 
-    //Recipe recipe;
+    Recipe recipe;
+
+    TextView tvRecipeName;
+    ImageView ivRecipeImage;
+    TextView tvRecipeDirections;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
+
+        tvRecipeName = (TextView) findViewById(R.id.tvRecipeName);
+        ivRecipeImage = (ImageView) findViewById(R.id.ivRecipeImage);
+        tvRecipeDirections = (TextView) findViewById(R.id.tvRecipeDirections);
+        tvRecipeDirections.setMovementMethod(new ScrollingMovementMethod());
+
+        // unwrap recipe passed in via intent
+
+        // set title
+
     }
 }
