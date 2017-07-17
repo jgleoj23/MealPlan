45b5c53234a9293b41e909c309adc5261ac3d278
Joseph Gardi <jgardi@fb.com>
2017-07-17 15:15:55 -0700

    added tabs

--- app/src/main/java/joseph/com/mealplan/MainActivity.java
+++ app/src/main/java/joseph/com/mealplan/MainActivity.java
@@ -1,12 +1,55 @@
 package joseph.com.mealplan;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = getClass().getName();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
+    }
+
+    public class MainPagerAdapter extends FragmentPagerAdapter {
+
+        private List<String> tabs = Arrays.asList("Search", "Meal Plan");
+
+        public MainPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new SearchFragment();
+                case 1:
+                    return new MealPlanFragment();
+                default:
+                    throw new RuntimeException("position " + position + " is out of bounds");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs.get(position);
+        }
     }
 }
