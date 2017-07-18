f981e0fb79119458561feb91b3d2c2872feff846
Santiago Ospina <sospina@fb.com>
2017-07-18 14:29:18 -0700

    made favorites a fragment

--- app/src/main/java/joseph/com/mealplan/FavoritesFragments.java
+++ app/src/main/java/joseph/com/mealplan/FavoritesFragments.java
@@ -1,16 +1,22 @@
 package joseph.com.mealplan;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class FavoritesFragments extends AppCompatActivity {
+import butterknife.ButterKnife;
 
+public class FavoritesFragments extends Fragment {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_favorites, container, false);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_favorites);
-
+        ButterKnife.bind(this, view);
+        return view;
     }
+
 
 
 }
