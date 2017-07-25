0ea52b792bd79b9a29dfdec045a8fd80d9255358
Santiago Ospina <sospina@fb.com>
2017-07-24 17:11:48 -0700

    made it so same recipe can't be added multiple times to favorites (correctly this time)

--- app/src/main/java/joseph/com/mealplan/MainActivity.java
+++ app/src/main/java/joseph/com/mealplan/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -101,9 +102,13 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setCurrentItem(1);
         getGroceryListFragment().addGroceries(recipe.getIngredients());
     }
+    ArrayList<String> favorited = new ArrayList<String>();
 
     public void favorite(Recipe recipe) {
         Log.i(TAG, "add to favorites");
-        getFavoritesFragment().addFavorite(recipe);
+        if(!favorited.contains(recipe.getTitle())) {
+            getFavoritesFragment().addFavorite(recipe);
+            favorited.add(recipe.getTitle());
+        }
     }
 }
