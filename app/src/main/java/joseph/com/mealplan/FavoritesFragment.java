4270a34a0b2f6453dad713dcae9f690915b0e4d1
Santiago Ospina <sospina@fb.com>
2017-07-27 12:40:10 -0700

    can unfavorite on long click now

--- app/src/main/java/joseph/com/mealplan/FavoritesFragment.java
+++ app/src/main/java/joseph/com/mealplan/FavoritesFragment.java
@@ -1,15 +1,22 @@
 package joseph.com.mealplan;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,11 +24,11 @@ import io.realm.Realm;
 import joseph.com.mealplan.model.Recipe;
 
 public class FavoritesFragment extends Fragment {
-    @BindView(R.id.rvFavorites)
-    RecyclerView rvFavorites;
-    ResultsAdapter resultsAdapter = new ResultsAdapter();
-
+    @BindView(R.id.lvFavorites)
+    ListView lvFavorites;
     MainActivity mainActivity;
+    FavoritesAdapter favoritesAdapter;
+    ArrayList<Recipe> recipes = new ArrayList<>();
     private Realm realm = Realm.getDefaultInstance();
 
     public static FavoritesFragment newInstance(MainActivity mainActivity) {
@@ -36,59 +43,103 @@ public class FavoritesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_favorites, container, false);
         ButterKnife.bind(this, view);
         super.onCreate(savedInstanceState);
-        rvFavorites.setAdapter(resultsAdapter);
+        favoritesAdapter = new FavoritesAdapter(getContext(), recipes);
+        lvFavorites.setAdapter(favoritesAdapter);
         for (Recipe recipe : realm.where(Recipe.class).findAll()) {
-            if(recipe.getTitle().substring(0, 1).equals("*") && !favorited.contains(recipe.getTitle())) {
+            if (recipe.getTitle().substring(0, 1).equals("*") && !favorited.contains(recipe.getTitle())) {
                 showFavorite(recipe);
                 favorited.add(recipe.getTitle());
             }
         }
-        rvFavorites.setLayoutManager(new LinearLayoutManager(getContext()));
-
-//        for (Recipe recipe : realm.where(Recipe.class).findAll()) {
-//            favorited.add(recipe.getTitle().toString());
-//            resultsAdapter.recipes.add(recipe);
-//            resultsAdapter.notifyDataSetChanged();
-//        }
+        setupListViewListener();
 
         return view;
     }
 
+
+    private void setupListViewListener() {
+        lvFavorites.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            //If list entry is long clicked, delete entry
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int i, long l) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setTitle("Alert!!");
+                alert.setMessage("Are you sure you want to unfavorite this recipe?");
+                alert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Recipe recipe = recipes.get(i);
+                        realm.beginTransaction();
+                        recipe.deleteFromRealm();
+                        realm.commitTransaction();
+                        dialog.dismiss();
+                        recipes.remove(i);
+                        favoritesAdapter.notifyDataSetChanged();
+                    }
+                });
+                alert.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                alert.show();
+
+                return true;
+            }
+        });
+    }
+
     ArrayList<String> favorited = new ArrayList<String>();
 
-    public void showFavorite(final Recipe recipe){
-        if(!favorited.contains(recipe.getTitle())) {
-            resultsAdapter.recipes.add(recipe);
-            resultsAdapter.notifyDataSetChanged();
+    public void showFavorite(final Recipe recipe) {
+        if (!favorited.contains(recipe.getTitle())) {
+            favoritesAdapter.add(recipe);
+            favoritesAdapter.notifyDataSetChanged();
         }
     }
+
+
     public void addFavorite(final Recipe recipe) {
-        if(!favorited.contains(recipe.getTitle())) {
+        if (!favorited.contains(recipe.getTitle())) {
             realm.beginTransaction();
-            recipe.setTitle("*"+recipe.getTitle());
+            recipe.setTitle("*" + recipe.getTitle());
             realm.insertOrUpdate(recipe);
             realm.commitTransaction();
         }
     }
 
 
-    public class ResultsAdapter extends RecyclerView.Adapter<RecipeSearchViewHolder> {
+    public class FavoritesAdapter extends ArrayAdapter<Recipe> {
 
-        private List<Recipe> recipes = new ArrayList<>();
-
-        @Override
-        public RecipeSearchViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new RecipeSearchViewHolder(new RecipeSearchView(getContext()));
+        public FavoritesAdapter(Context context, ArrayList<Recipe> recipes) {
+            super(context, 0, recipes);
         }
 
         @Override
-        public void onBindViewHolder(RecipeSearchViewHolder holder, int position) {
-            holder.getRecipeView().bind(recipes.get(position));
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Get the data item for this position
+            Recipe recipe = getItem(position);
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_recipe, parent, false);
+            }
+            TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            ImageView ivPic = (ImageView) convertView.findViewById(R.id.ivPic);
+                // Lookup view for data population
+            tvTitle.setText(recipe.getTitle());
+            Picasso.with(getContext())
+                        .load(recipe.getImageUrl())
+                        .fit().centerCrop()
+                        .into(ivPic);
+
+
+            return convertView;
         }
 
-        @Override
-        public int getItemCount() {
-            return recipes.size();
-        }
+
     }
 }
