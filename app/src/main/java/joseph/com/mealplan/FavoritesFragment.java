772d6b71f453e6a8a6485e9352b1b32c0b6257cf
Santiago Ospina <sospina@fb.com>
2017-07-27 13:02:25 -0700

    implemented circular favorite images

--- app/src/main/java/joseph/com/mealplan/FavoritesFragment.java
+++ app/src/main/java/joseph/com/mealplan/FavoritesFragment.java
@@ -3,6 +3,10 @@ package joseph.com.mealplan;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
@@ -16,6 +20,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import org.parceler.Parcels;
 
@@ -147,12 +152,46 @@ public class FavoritesFragment extends Fragment {
             Picasso.with(getContext())
                         .load(recipe.getImageUrl())
                         .fit().centerCrop()
+                        .transform(new CircleTransform())
                         .into(ivPic);
 
 
             return convertView;
         }
+        public class CircleTransform implements Transformation {
+            @Override
+            public Bitmap transform(Bitmap source) {
+                int size = Math.min(source.getWidth(), source.getHeight());
 
+                int x = (source.getWidth() - size) / 2;
+                int y = (source.getHeight() - size) / 2;
 
+                Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+                if (squaredBitmap != source) {
+                    source.recycle();
+                }
+
+                Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+                Canvas canvas = new Canvas(bitmap);
+                Paint paint = new Paint();
+                BitmapShader shader = new BitmapShader(squaredBitmap, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+                paint.setShader(shader);
+                paint.setAntiAlias(true);
+
+                float r = size/2f;
+                canvas.drawCircle(r, r, r, paint);
+
+                squaredBitmap.recycle();
+                return bitmap;
+            }
+
+            @Override
+            public String key() {
+                return "circle";
+            }
+        }
     }
+
+
 }
